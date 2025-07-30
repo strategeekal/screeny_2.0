126c80989d0354a08fd03ea443a5d56dbdfc4b48
DIEGO CELAYETA <diegocelayeta@gmail.com>
2025-07-30 13:24:40 -0500

    Inluded functionality to connect top internet. working but needs more validation

--- code.py
+++ code.py
@@ -19,6 +19,8 @@ import adafruit_ds3231 # RTC board library
 import time # Time and timing related functions
 import adafruit_ntp # Network Time Protocol (NTP) helper for CircuitPython
 
+# ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== #
+
 ### INITIALIZE REAL TIME CLOCK ###
 
 ## Initialize the I2C Bus (For RTC functionality connected via STEMA from DS3231 Board):
@@ -38,4 +40,52 @@ for attempt in range(10):
 else:
 	supervisor.reload()
 
-print("Hello!!!")
+# ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== # # ====== #
+
+### CONNECT TO INTERNET ###
+
+## GET AND CONFIRM CREDENTIALS 
+
+ssid = os.getenv("CIRCUITPY_WIFI_SSID")
+password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
+aio_username = os.getenv("ADAFRUIT_AIOS_USERNAME")
+aio_key = os.getenv("ADAFRUIT_AIO_KEY")
+timezone = os.getenv("TIMEZONE")
+TIME_URL = f"https://io.adafruit.com/api/v2/{aio_username}/integrations/time/strftime?x-aio-key={aio_key}&tz={timezone}"
+TIME_URL += "&fmt=%25Y-%25m-%25d+%25H%3A%25M%3A%25S.%25L+%25j+%25u+%25z+%25Z"
+
+## CHECK CREDENTIALS
+if any(var is None for var in (ssid, password, aio_username, aio_key, timezone)):
+	recurr_message = "WiFI Key"
+else:
+	recurr_message = "Keys Imported"
+
+# welcome_label.text = recurr_message
+print(recurr_message)
+
+## ESTABLISH A CONNECTION
+for attempt in range(10):
+	try:
+		wifi.radio.connect(ssid, password)
+		print(f"Connected to {ssid}")
+		print(f"My IP address: {wifi.radio.ipv4_address}")
+		break
+	except ConnectionError as e:
+		print(f"Connection Error: {e}")
+		print("Attempt {} of 10, Retrying in 5 seconds...".format(attempt))
+		time.sleep(5)
+		continue
+else:
+	supervisor.reload()
+
+# ping_ip = ipaddress.IPv4Address("8.8.8.8")
+# ping = wifi.radio.ping(ip=ping_ip)
+# 
+# if ping is None:
+# 	ping = wifi.radio.ping(ip=ping_ip)
+# 
+# if ping is None:
+# 	print("Couldn't ping 'google.com' successfully")
+# else:
+# 	# convert s to ms
+# 	print(f"Pinging 'google.com' took: {ping * 1000} ms")
