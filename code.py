3e84ba45be818ed375bba622341e14870fba2a20
DIEGO CELAYETA <diegocelayeta@gmail.com>
2025-08-01 11:43:56 -0500

    commiting some last minute changes

--- code.py
+++ code.py
@@ -203,9 +203,9 @@ def get_chicago_time_from_ntp():
 
 
 ## UPDATE RTC ##
-rtc.datetime = time.struct_time((2017, 1, 1, 0, 0, 0, 6, 1, -1))
 
-print(rtc.datetime)
+# rtc.datetime = time.struct_time((2017, 1, 1, 0, 0, 0, 6, 1, -1)) # FOR TEST
+# print(rtc.datetime) # FOR TEST
 
 chicago_time = get_chicago_time_from_ntp()
 
